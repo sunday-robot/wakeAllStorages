e4247d103c3b7a44a45403dc198516c36eb3c9cd
sunday_robot <sgx03150@nifty.com>
2024-10-11 19:56:47 +0900

    a

---
+++ wakeAllStorages/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace wakeAllStorages
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+            while (true)
+            {
+                Console.WriteLine($"{DateTime.Now}");
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                foreach (var drive in drives)
+                {
+                    // エントリーの列挙をすればドライブがスリープしないと思っている。(根拠はない。)
+                    var entries = Directory.EnumerateFileSystemEntries(drive.Name).ToArray();
+                    Console.WriteLine($"{drive.Name} : number of entries = {entries.Length}");
+                }
+                Console.WriteLine("");
+
+                Thread.Sleep(60 * 1000);    // 1分間隔で実行する。
+            }
+        }
+    }
+}
